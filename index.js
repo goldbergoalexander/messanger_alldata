303cd48c58d6f7ad48a515dbdaa7f1602f716a30
root <alexandergoldberg@ukr.net>
2019-03-26 13:15:38 +0000

    Verify token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 require('dotenv').config();
+const axios  = require('axios');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const SEND_API = process.env.SEND_API;
 // Imports dependencies and set up http server
