9a681fbc5d3d9a3016ce237049099547f3d8fd1c
root <alexandergoldberg@ukr.net>
2019-03-26 07:39:53 +0000

    start app

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function handleMessage(sender_psid, received_message) {
 /**
  * Calls the Messenger API to send the message
  */
+ /*
 function callSendAPI(psid, message) {
     let data = { 
         "recipient": { "id": psid }, 
@@ -48,6 +49,32 @@ function callSendAPI(psid, message) {
         }
     });
 }
+*/
+function callSendAPI(sender_psid, response) {
+// Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+ // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+
+  
+}
+
 
 
 /**
