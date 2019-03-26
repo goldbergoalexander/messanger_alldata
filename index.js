08a649f45b2e1d9b00549c1f3a1101690749c412
root <alexandergoldberg@ukr.net>
2019-03-26 13:18:04 +0000

    Verify token

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function handleMessage(sender_psid, received_message) {
 let response;
 let text = received_message.text;
 
- if (received_message.text) {
+ if (received_message.text==="hello") {
 	  	   response = { "text": "hi there, i am AllDataBot" }
 
 }
