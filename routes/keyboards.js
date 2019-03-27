47207ed7ed20e6825c22fa7b01a9087dea8d4456
root <alexandergoldberg@ukr.net>
2019-03-27 09:43:43 +0000

    add image

--- routes/keyboards.js
+++ routes/keyboards.js
@@ -71,7 +71,6 @@ bodyParser = require('body-parser');
       "subtitle":"Cool",
       "default_action": {
         "type": "web_url",
-        "url": "https://github.com/goldbergoalexander/messanger_alldata/blob/master/img/search.png",
         "messenger_extensions": "TRUE",
         "webview_height_ratio": "COMPACT"
       },
